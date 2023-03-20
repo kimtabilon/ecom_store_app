bd5800d29a6ad90130902863f822b9b9e903be2e
Kim Tabilon <kim.gtabilon@gmail.com>
2023-03-20 20:55:13 +0800

    updates

--- lib/Constants/url.dart
+++ lib/Constants/url.dart
@@ -1,5 +1,4 @@
 class AppUrl {
-  static String baseUrl = 'https://wisdommatt-todolist-api.herokuapp.com';
   static String storeUrl = "dev.ecommercebusinessprime.com";
   static String consoleUrl = "ebpconsole.ecommercebusinessprime.com";
 }
