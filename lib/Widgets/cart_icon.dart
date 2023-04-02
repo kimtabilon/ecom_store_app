d498a7d8cc8efda031bec11e47894e015167aca7
Kim Tabilon <kim.gtabilon@gmail.com>
2023-04-03 07:39:03 +0800

    added pages: about us, help center

---
+++ lib/Widgets/cart_icon.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../Provider/StoreProvider/cart_provider.dart';
+import '../Screens/Account/cart_page.dart';
+import '../Utils/routers.dart';
+import 'package:badges/badges.dart' as badges;
+
+class CartIconWidget extends StatefulWidget {
+  const CartIconWidget({Key? key}) : super(key: key);
+
+  @override
+  State<CartIconWidget> createState() => _CartIconWidgetState();
+}
+
+class _CartIconWidgetState extends State<CartIconWidget> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      Provider.of<CartProvider>(context, listen: false).refreshCartTotal();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+        onTap: () {
+          PageNavigator(ctx: context).nextPage(page: const CartPage());
+        },
+
+        child: Consumer<CartProvider>(
+            builder: (context, cart, child) {
+              return cart.cart_total_items != '' && cart.cart_total_items != '0'
+                  ? Padding(
+                padding: const EdgeInsets.fromLTRB(0, 15, 0, 0),
+
+                child: badges.Badge(
+                  badgeContent: Text(
+                    cart.cart_total_items,
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                    child: Icon(Icons.shopping_cart_outlined,
+                        size: 28, color: Colors.lightGreen),
+                  ),
+                ),
+              )
+                  : const Padding(
+                padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                child: Icon(
+                    Icons.shopping_cart_outlined,
+                    size: 28,
+                    color: Colors.lightGreen
+                ),
+              );
+            })
+    );
+  }
+}
---
+++ lib/Widgets/search_field.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:page_transition/page_transition.dart';
+import '../Screens/Common/categories_screen.dart';
+import '../Screens/Common/feeds_screen.dart';
+
+import 'appbar_icons.dart';
+
+class SearchFieldWidget extends StatefulWidget {
+  const SearchFieldWidget({Key? key}) : super(key: key);
+
+  @override
+  State<SearchFieldWidget> createState() => _SearchFieldWidgetState();
+}
+
+class _SearchFieldWidgetState extends State<SearchFieldWidget> {
+  final TextEditingController _searchText = TextEditingController();
+
+  /*@override
+  void dispose() {
+    super.dispose();
+  }*/
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.all(0),
+          child: TextField(
+            controller: _searchText,
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20.0),
+                borderSide: BorderSide(width: 0.8),
+              ),
+              hintText: "Search",
+              prefixIcon: AppBarIcons(
+                function: () {
+                  Navigator.push(
+                    context,
+                    PageTransition(
+                      type: PageTransitionType.fade,
+                      child: const CategoriesScreen(),
+                    ),
+                  );
+                },
+                icon: IconlyBold.category,
+              ),
+              suffixIcon: Row(
+                mainAxisAlignment:
+                MainAxisAlignment.spaceBetween, // added line
+                mainAxisSize: MainAxisSize.min, // added line
+                children: <Widget>[
+                  IconButton(
+                    icon: const Icon(
+                      IconlyLight.search,
+                      color: Colors.green,
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => FeedsScreen(
+                                target: _searchText.text.trim(),
+                                itemSearch: 'true')),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+            onSubmitted: (String str) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => FeedsScreen(
+                        target: _searchText.text.trim(),
+                        itemSearch: 'true')),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
